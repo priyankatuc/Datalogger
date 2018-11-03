1a93b73c660c8b30dad2feef41fefe3b2277f6c4
priyankatuc <44735819+priyankatuc@users.noreply.github.com>
2018-11-03 23:20:50 +0100

    Add files via upload
    Still working on it

---
+++ data2.py
@@ -0,0 +1,15 @@
+import os
+import time 
+from time import sleep
+from datetime import datetime
+
+file = open("/home/pi/data_log2.csv", "a")
+i=0
+if os.stat("/home/pi/data_log.csv").st_size == 0:
+        file.write("Time,Sensor1,Sensor2,Sensor3,Sensor4,Sensor5\n")
+while True:
+        i=i+1
+        now = datetime.now()
+        file.write(str(now)+","+str(i)+","+str(-i)+","+str(i-10)+","+str(i+5)+","+str(i*i)+"\n")
+        file.flush()
+        time.sleep(5)
